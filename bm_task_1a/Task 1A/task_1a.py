88b2fecaad27fb070e7fb5d85d17a4620df47a06
kanikaarya262 <93133854+kanikaarya262@users.noreply.github.com>
2021-10-27 13:28:39 +0530

    Update task_1a.py

--- bm_task_1a/Task 1A/task_1a.py
+++ bm_task_1a/Task 1A/task_1a.py
@@ -66,6 +66,46 @@ def detect_shapes(img):
 	detected_shapes = []
 
 	##############	ADD YOUR CODE HERE	##############
+	def detect_shapes(img):
+		detected_shapes=[]
+                blue=np.where((img == [255,0,0]).all(axis = 2))
+                green=np.where((img == [0,255,0]).all(axis =  2))
+                red=np.where((img == [0,0,255]).all(axis = 2))
+                orange=np.where((img==[0,165,255]).all(axis=2))
+                img1gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+                ret, thresh = cv2.threshold(img1gray,180,255,cv2.THRESH_BINARY)
+                cv2.imshow("thresholded image",thresh)
+                cont , hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                for i in range(1,len(cont)):
+			Cent = cv2.moments(cont[i])
+                        cx =int(Cent["m10"]/Cent["m00"])
+                        cy =int(Cent["m01"]/Cent["m00"])
+                        if cy in blue[0] and cx in blue[1]:
+				color='Blue'
+                        elif cy in green[0] and cx in green[1]:
+				color='Green'
+                        elif cy in red[0] and cx in red[1]:
+				color='Red'
+                        elif cy in orange[0] and cx in orange[1]:
+                                color='Orange'
+                        app = cv2.approxPolyDP(cont[i],0.01*cv2.arcLength(cont[i],True),True)
+                        if len(app) == 3:
+				shape = 'Triangle'
+			elif len(app)==4:
+				(x,y,a,b)=cv2.boundingRect(app)
+				if(a/b==1):
+					shape='Square'
+				else:
+					shape = 'Rectangle'
+			elif len(app)==5:
+				shape = 'Pentagon'
+			elif len(app)==6:
+				shape = 'Hexagon'
+			else:
+				shape ='Circle'
+			li1=[color,shape,(cx,cy)]
+                        detected_shapes.append(li1)
+		return detected_shapes
 
 	
 	 
